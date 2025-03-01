308055c1ea9ebf7f10709b6ded0a1dd4ae2236ae
tsuchiya-d <tsuchiya-d@aidemy.co.jp>
2025-03-01 18:32:30 +0900

    fix: テンプレートをバイナリに組み込むように修正

--- scripts/build-and-pack.sh
+++ scripts/build-and-pack.sh
@@ -3,11 +3,11 @@ set -e
 
 # 1. Build for ARM64
 cargo clean
-cargo build --target aarch64-apple-darwin
+cargo build --release --target aarch64-apple-darwin
 
 # 2. Copy the binary to the npm package
-cp target/aarch64-apple-darwin/debug/raphook packages/npm/raphook/bin/
-cp target/aarch64-apple-darwin/debug/raphook packages/npm/raphook-darwin-arm64/bin/
+cp target/aarch64-apple-darwin/release/raphook packages/npm/raphook/bin/
+cp target/aarch64-apple-darwin/release/raphook packages/npm/raphook-darwin-arm64/bin/
 
 # 3. Change directory to npm package and run npm pack
 cd packages/npm/raphook
